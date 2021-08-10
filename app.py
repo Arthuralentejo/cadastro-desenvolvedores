d53ee5ec14f85cb73d167d09bf42412a28e1dd24
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-08-10 16:31:34 -0300

    Criando db e adicionando metodos

--- app.py
+++ app.py
@@ -3,72 +3,42 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("cadastro.html")
+def index():
+    return render_template("index.html")
 
 @app.route('/send_data', methods = ['POST'])
 def send_data():
+    connection = sqlite3.connect('devs.db')
     nome = request.form['nome']
     habilidade = request.form['habilidade']
-    con = sqlite3.connect('devs.db')
-    cur = con.cursor()
-    cur.execute("INSERT INTO devs (nome, habilidades) VALUES (?,?)", (nome, habilidade))
-    con.commit()
-    con.close()
-    return render_template("cadastro.html")
+    c = connection.cursor()
+    c.execute("INSERT INTO devs (nome, habilidades) VALUES (?,?)", (nome, habilidade))
+    connection.commit()
+    return render_template("index.html")
 
 @app.route('/show_data', methods = ['GET'])
 def show_data():
-    con = sqlite3.connect('devs.db')
-    cur = con.cursor()
-    show = cur.execute("SELECT * FROM devs")
-    con.commit()
-    con.close()
-    return render_template("cadastro.html", **{'devs':show})
+    connection = sqlite3.connect('devs.db')
+    c = connection.cursor()
+    c.execute("SELECT nome, habilidades FROM devs")
+    nomes = c.fetchall()
+
+    connection.commit()
+    return render_template("index.html", nomes = nomes)
+
+@app.route('/del_data', methods = ['POST'])
+def del_data():
+    connection = sqlite3.connect('devs.db')
+    c = connection.cursor()
+    dev = request.form['delnome']
+    c.execute("DELETE FROM devs WHERE nome = ?",(dev,))
+    c.fetchall()
+    connection.commit()
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# desenvolvedores = [
-#     {
-#         'nome':'Arthur',
-#         'habilidades': ['Python', 'Flask']
-#      },
-#     {
-#         'nome':'Alex',
-#         'habilidades': ['Python', 'Django']}
-# ]
-
-# devolve um desenvolvedor pelo ID, também altera e deleta um desenvolvedor
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 python -m venv dev_api 
 .\scripts\activate
 pip install Flask
-pip install db-sqlite3
-run app.py
